5983357fbb7aa0e87f693dccbd952ab1debefa4a
Awena SALIC <asalic@f4r2s5.clusters.42paris.fr>
2026-03-09 15:29:51 +0100

    fin de projet

--- app/components/RegisterActivity.tsx
+++ app/components/RegisterActivity.tsx
@@ -1,5 +1,7 @@
 import { Card, CardContent } from "@/components/ui/card";
 import { ColorsPanel } from "../page";
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
 
 type ActivityInfos = {
     picturePath: string;
@@ -15,29 +17,72 @@ interface ActivityProps {
     colors: ColorsPanel;
 }
 
+type Logs = {
+    type: "loading" | "empty" | "success" | "error";
+    message?: string;
+}
+
 export default function RegisterActivity({activityInfos, colors}: ActivityProps) {
+    const [logs, setLogs] = useState<Logs>({type: "empty"});
 
+    const dateFormat = new Date(activityInfos.date).toLocaleDateString("fr-FR", {
+        weekday: "short",
+        day: "2-digit",
+        month: "short",
+        year: "numeric",
+    });
+    const capitalizeDate = dateFormat.replace(/^./, dateFormat[0].toUpperCase());
+
+    if (logs.type === "loading" ) {
+        return (
+            <div><p>Loading...</p></div>
+        );
+    } else {
     return (
-        <div className="h-[20rem] flex flex-col justify-center items-center border-2 rounded-lg py-5 px-5 bg-white max-sm:max-h-[20rem]">
-            <div className="text-[var(--font-color)]">
 
-                <div>
-                    <div className="relative">
-                    <img src={activityInfos.picturePath} />
+        <div className={`h-[20rem] flex flex-col shadow-md border-2 rounded-lg bg-white cursor-pointer transition-transform transform hover:scale-101 max-sm:max-h-[20rem]`}
+        style={{ borderColor: `${colors.titleColor}`}}>
+            <div className="text-sm">
 
+                <div className="relative h-35 ">
+                    <img className="rounded-t-md object-cover w-full h-full" title={activityInfos.title} src={activityInfos.picturePath} />
+
+                    <div className="absolute bottom-0 w-full backdrop-blur-sm p-1 "
+                    style={{ backgroundColor: "rgba(255,255,255,0.5)" }}>
+                        <h2 className={`font-semibold text-base text-[${colors.titleColor}]`}>{activityInfos.title}</h2>
                     </div>
-                    <h2 className={`text-[${colors.primaryColor}]`}>{activityInfos.title}</h2>
                 </div>
 
-                <p>{activityInfos.location}</p>
-                <p>{activityInfos.price}</p>
-                <p>{activityInfos.date}</p>
+                <div className="flex flex-1 flex-col p-2">
 
-                <button>Reserver</button>
+                    <div className={`flex flex-col h-full text-[${colors.secondaryColor}]`}>
+                        <p>📍{activityInfos.location}</p>
+                        <p>🪙 {activityInfos.price}€</p>
+                        <p>🗓️ {capitalizeDate}</p>
+                    </div>
 
-                <p>{activityInfos.places_left} places restantes</p>
+                    <div className="flex flex-col justify-center mt-5">
+                        <button
+                            disabled={activityInfos.places_left <= 0}
+                            onClick={() => {
+                                setLogs({type:"success", message: "Vous avez réservé cet atelier 🍾"});
+                                if (activityInfos.places_left > 0 ) activityInfos.places_left --;
+                            }}
+                            className={`text-white p-1 rounded-lg border-1 transition-transform transform hover:scale-101 hover:border-black`}
+                            style={{ backgroundColor: `${colors.primaryColor}`}}
+                        >
+                            Reserver
+                        </button>
+                        {logs.type === "success" || logs.type === "error" && <p className={`text-${logs.type === "error" ? "red-400" : '[' + colors.titleColor + ']'} text-xs text-center`}>{logs.message}</p>}
+                    </div>
 
+                    <div className={`flex justify-center mt-2 text-[${colors.secondaryColor}]`}>
+                        <p>👥 {activityInfos.places_left} places restantes</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
+    }
+
 } 
--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,37 @@
+'use client'
 import Image from "next/image";
 import RegisterActivity from "./components/RegisterActivity";
 import activities from "./data/activities.json";
 
 import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious } from "@/components/ui/carousel";
-import { Card, CardContent } from "@/components/ui/card";
 
 export type ColorsPanel = {
     backgroundColor: string;
     primaryColor: string;
     secondaryColor: string;
+    titleColor: string;
 }
 
 export default function Home() {
-
   const activityColors : ColorsPanel = {
     backgroundColor: "var(--background-color)",
     primaryColor: "var(--primary-color)",
     secondaryColor: "var(--font-color)",
+    titleColor: "var(--accent-color)",
   }
 
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[var(--color-background)]">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-25 px-10 bg-[var(--color-background)] sm:items-start">
-        <div className="flex flex-col items-start gap-6 text-left w-full border-b-2 pb-4">
-          <h1 className="text-4xl font-bold">
-            Daisy App
-          </h1>
-        </div>
-        <div className="flex flex-col justify-center items-center text-center max-sm:items-start max-sm:text-left">
+    
+    <div className="relative z-10 flex flex-col min-h-screen items-center justify-center py-10 px-5 bg-[var(--color-background)]">
+
+      <header className="flex flex-col items-start gap-6 text-left w-full border-b-2 pb-4">
+        <h1 className="text-4xl font-bold">
+          Daisy App
+        </h1>
+      </header>
+        
+      <main className="flex flex-1 w-full flex-col justify-center py-10 bg-[var(--color-background)] gap-5">
+        <div className="flex flex-col justify-center items-center text-center">
           <h1 className="max-w-sm text-2xl font-semibold tracking-tight text-[var(--accent-color)]">
             Reservez un atelier
           </h1>
@@ -36,17 +40,17 @@ export default function Home() {
           </p>
         </div>
 
-        <div className="flex w-full flex-row justify-center gap-4 text-base font-medium">
+        <div className="flex w-full flex-row justify-center gap-4 text-base font-medium px-4">
          
           <Carousel
             opts={{
               align: "start",
             }}
-            className="w-full max-w-[25rem] max-sm:max-w-[20rem]"
+            className="w-full max-w-[30rem] max-sm:max-w-[15rem] lg:max-w-[50rem]"
           >
             <CarouselContent>
-              {activities.map((activity) => (
-                <CarouselItem className="basis-1/2 lg:basis-1/3">
+              {activities.map((activity, index) => (
+                <CarouselItem key={index} className="basis-1/2 max-sm:basis-1/1 lg:basis-1/3 ">
                   <div className="p-1">
                       <RegisterActivity activityInfos={activity} colors={activityColors}/>
                   </div>
@@ -58,9 +62,6 @@ export default function Home() {
           </Carousel>
             
         </div>
-
-        <div></div>
-        <div></div>
 
       </main>
     </div>
